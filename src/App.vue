e2fc4aac055a512e68b87db2a78f3837dee7fe95
Mars <zm03322161@qq.com>
2019-08-15 21:07:50 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+    
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import './plugins/vant.js'
 import i18n from './i18n'
+// 导入 AOS 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 Vue.config.productionTip = false
 
@@ -11,5 +14,8 @@ new Vue({
   router,
   store,
   i18n,
+  created () {
+    AOS.init()
+  },
   render: h => h(App)
 }).$mount('#app')
